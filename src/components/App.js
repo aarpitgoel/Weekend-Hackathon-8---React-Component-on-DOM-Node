08a5d2d11a05cfcc4e39681825c116074b7e37d2
aarpitgoel <64373216+aarpitgoel@users.noreply.github.com>
2020-12-13 18:21:35 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
-const App = () => {
+export default function App() {
   return (
     <div>
       Now I can render any React component on any DOM node I want using
       ReactDOM.render
     </div>
   );
-};
-
-export default App;
+}
